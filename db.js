e6917658cd455c68f8723fb923aa8974ada96e10
Diazssj17 <Diazgomezalejandro8@gmail.com>
2025-10-09 19:41:49 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-import mysql from 'mysql2';
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST || 'mysql.railway.internal',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'JogTRBVozjGbMnPNOSxkBQbWjSpySKld',
+  database: process.env.MYSQLDATABASE || 'railway',
+  port: process.env.MYSQLPORT || 3306
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('❌ Error de conexión a MySQL:', err);
-  } else {
-    console.log('✅ Conectado correctamente a MySQL (Railway)');
+    console.error('❌ Error conectando a MySQL:', err);
+    return;
   }
+  console.log('✅ Conectado correctamente a MySQL');
 });
 
-export default connection;
+module.exports = connection;
